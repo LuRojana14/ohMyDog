a988776296fa9b6d66262ab4c6a6a9ac5d320ff6
Lurojana14 <lourdes_rojana@hotmail.com>
2020-08-06 12:55:34 +0200

    cambio en el filtro, ya funciona, mas el chat

--- routes/users.js
+++ routes/users.js
@@ -15,12 +15,21 @@ router.use((req, res, next) => {
 
 //SHOW ALL DOGS IN HOME PRIVATE PAGE
 router.get("/homeprivate", (req, res, next) => {
-  Dog.find()
+
+  let breed = req.query.breed
+  let data = {}
+  if(breed){
+    data = {
+      breed: breed
+    }
+  }
+  console.log(data)
+  Dog.find(data)
     .then((allTheDogFromDB) => {
       res.render("homeprivate", { alldogs: allTheDogFromDB });
     })
     .catch((error) => {
-      console.log("Error while getting the books from the DB: ", error);
+      console.log("Error while getting the dogs from the DB: ", error);
     });
 });
 
